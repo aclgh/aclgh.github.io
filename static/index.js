3f5c5df53c3684d503831147764580f561208b60
aclgh <60092080+aclgh@users.noreply.github.com>
2022-01-29 11:42:52 +0800

    Update index.js

--- static/index.js
+++ static/index.js
@@ -441,9 +441,9 @@ const MODE_NORMAL = 1, MODE_ENDLESS = 2, MODE_PRACTICE = 3;
             SubmitResults();
         }
 
-        if (cps <= 5) return '试着好好练一下？';
-        if (cps <= 8) return 'TCL';
-        if (cps <= 10)  return 'TQL';
+        if (cps <= 5) return '傻逼';
+        if (cps <= 8) return '下面的交给数学！';
+        if (cps <= 10)  return '连小木块都找不好找什么对象？';
         if (cps <= 15) return '您';
         return '人？';
     }
