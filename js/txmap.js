1c8fd168108204456223e67c6e87fa86145c90aa
aclgh <1634856781@qq.com>
2023-01-07 16:11:35 +0800

    Site updated: 2023-01-07 16:11:30

--- js/txmap.js
+++ js/txmap.js
@@ -220,4 +220,6 @@ function showWelcome() {
         // console.log("Pjax无法获取#welcome-info元素🙄🙄🙄")
     }
 }
-showWelcome();
+window.onload = showWelcome;
+// 如果使用了pjax在加上下面这行代码
+document.addEventListener('transitionend', showWelcome);
