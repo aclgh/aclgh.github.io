b2ae8e9f7ae6e05ddbc8d7398f31c730259f7992
aclgh <heroaclgh@gmail.com>
2024-11-20 00:38:14 +0800

    评论

--- src/config.ts
+++ src/config.ts
@@ -84,6 +84,6 @@ export const licenseConfig: LicenseConfig = {
 }
 export const commentConfig: CommentConfig = {
   twikoo: {
-    envId: 'https://twikoo.aclgh.top',
+    envId: 'http://aclgh.top:40063/',
   },
 }
