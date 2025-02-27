c14da4b7092030cd7ace6c6fe203d0055d6036d2
aclgh <heroaclgh@gmail.com>
2025-02-27 21:53:18 +0800

    change banner

--- src/config.ts
+++ src/config.ts
@@ -46,7 +46,7 @@ export const navBarConfig: NavBarConfig = {
     LinkPreset.Friends,
     {
       name: 'GitHub',
-      url: 'https://github.com/saicaca/fuwari',     // Internal links should not include the base path, as it is automatically added
+      url: 'https://github.com/aclgh',     // Internal links should not include the base path, as it is automatically added
       external: true,                               // Show an external link icon and will open in a new tab
     },
   ],
